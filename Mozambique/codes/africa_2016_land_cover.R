73f8be7cbfc99852bbe359f3fd893f3321c30bc6
Abdollah Jalilian <stat4aj@gmail.com>
2023-09-28 14:11:23 +0100

    Update africa_2016_land_cover.R

--- Mozambique/codes/africa_2016_land_cover.R
+++ Mozambique/codes/africa_2016_land_cover.R
@@ -1,29 +1,30 @@
 
-# S2 prototype Land Cover 20m map of Africa 2016
-# https://2016africalandcover20m.esrin.esa.int/
-# available to the public by the ESA Climate Change Initiative and 
-#     its Land Cover project as the source of the CCI-LC database
-# copyright
+# S2 Prototype Land Cover 20m Map of Africa 2016
+# Source: https://2016africalandcover20m.esrin.esa.int/
+# Provided to the public by the ESA Climate Change Initiative and 
+#      its Land Cover project as the source of the CCI-LC database
+# Copyright Notice:
 # © Contains modified Copernicus data (2015/2016)
 # © ESA Climate Change Initiative - Land Cover project 2017
 
 # format: GeoTIFF
-# value codes
-# 0	No data
-# 1	Tree cover areas
-# 2	Shrubs cover areas
-# 3	Grassland
-# 4	Cropland
-# 5	Vegetation aquatic or regularly flooded
-# 6	Lichens Mosses / Sparse vegetation
-# 7	Bare areas
-# 8	Built up areas
-# 9	Snow and/or Ice
-# 10	Open Water
+
+# value codes:
+# 0   No data
+# 1   Tree cover areas
+# 2   Shrubs cover areas
+# 3   Grassland
+# 4   Cropland
+# 5   Vegetation aquatic or regularly flooded
+# 6   Lichens Mosses / Sparse vegetation
+# 7   Bare areas
+# 8   Built-up areas
+# 9   Snow and/or Ice
+# 10  Open Water
 
 # =========================================================
 
-# Create a temporary directory to download map data
+# create a temporary directory to extract the download land cover data
 temp_dir <- "/tmp/africa2016landcover/"
 dir.create(temp_dir)
 setwd(temp_dir)
@@ -35,31 +36,19 @@ downloaded_file <-
 # extract the downloaded file
 unzip(zipfile=downloaded_file, exdir=temp_dir)
 
-# read the africa 2016 land cover
+# read the Africa 2016 land cover using the 'terra' package
 library("terra")
-africa_land <- 
+africa_land_cover <- 
   rast(paste0(temp_dir, 
                 "ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0.tif"))
 
-# get Mozambique shapefile
-download.file(
-  "https://geodata.ucdavis.edu/gadm/gadm4.1/shp/gadm41_MOZ_shp.zip",
-  destfile="Moz.zip")
+# plot the Africa 2016 land cover map
+plot(africa_land_cover, type="classes")
 
-# extract Mozambique shapefile
-unzip(zipfile="Moz.zip", exdir=temp_dir)
+# function to extract land cover type by coordinates
+get_land_cover <- function(x, y)
+{
+  extract(africa_land_cover, cbind(x, y))
+}
 
-# read the shapefile of Mozambique boundary
-library("sf")
-moz_map <- read_sf(temp_dir, 
-                   layer="gadm41_MOZ_0")
-
-# crop the raster map of Africa 2016 lancover to Mozambique map
-
-moz_land <- 
-  terra::crop(africa_land, 
-              ext(moz_map), snap="near")
-
-library("tidyverse")
-africa_land %>%
-  ggplot() + geom_raster()
+# =========================================================
