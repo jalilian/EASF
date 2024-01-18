4b71fb31ee9c7563bd42efaf56e91ae5317623d8
Abdollah Jalilian <stat4aj@gmail.com>
2024-01-18 10:05:20 +0000

    Update read_data.R

--- Ghana/codes/read_data.R
+++ Ghana/codes/read_data.R
@@ -3,6 +3,49 @@ library("sf")
 library("tidyverse")
 library("readxl")
 
+gha_coor <- read_excel(
+  "~/Downloads/Ghana_coordinates.xlsx",
+  sheet="Sheet1",
+  range="A1:Q81",
+  )
+  
+
+coortfun <- function(x)
+{
+  y <- strsplit(x, "°")
+  y <- lapply(y, function(o){ gsub("[^0-9.]", "", o) })
+  out <- unlist(lapply(y, function(o){
+    if (length(o) == 1)
+    {
+      as.numeric(o)
+    } else{
+      y1 <- o[1]
+      y2 <- o[2]
+      if (y2 == "")
+      {
+        as.numeric(y1)
+      } else{
+        as.numeric(y1) + as.numeric(y2) / 60
+      }
+    }
+  }))
+  out <- ifelse(grepl("(-|W|S)", x), -out, out)
+  return(out)
+}
+
+gh_coor <- gha_coor %>% 
+  #filter(str_detect(latitude, "N ")) %>%
+  mutate(lat=coortfun(latitude),
+         long=coortfun(longitude),
+         )
+
+gh_coor %>%
+  select(`house ID`, latitude, lat, longitude, long)%>%
+  print(n=500)
+
+gh_coor %>% 
+  write_csv(file="~/Desktop/GHA_coordinates.csv")
+
 gha_data <- 
   read_excel("~/Downloads/Ghana EASF data_03Jan24.xlsx", 
              sheet="Sheet 1 - events",
@@ -21,3 +64,9 @@ download.file(
   destfile="gha.zip")
 unzip("gha.zip", exdir="gha/")
 gha_map <- read_sf("gha/", layer="gadm41_GHA_2")
+
+gha_map %>% count(NAME_1)
+gha_map %>% count(NAME_2)
+
+
+gha_data %>% select(longitude, latitude) %>% distinct() %>% points()
