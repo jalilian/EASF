7258b116eb0f290d82318399f78c3f4ad5139977
Abdollah Jalilian <stat4aj@gmail.com>
2024-09-04 12:45:59 +0100

    Update gha_sentinel_comparison.R

--- Ghana/codes/gha_sentinel_comparison.R
+++ Ghana/codes/gha_sentinel_comparison.R
@@ -8,4 +8,4 @@ sdata <- list()
 sdata[[1]] <- read_excel("~/Downloads/Ghana/Control sites HLC.xlsx",
                          sheet="Sheet 1 - events 2")
 sdata[[2]] <- read_excel("~/Downloads/Ghana/Control sites CID.xlsx",
-                         sheet="Sheet 1 - events")
+                         sheet="Sheet 1 - events", skip=1)
