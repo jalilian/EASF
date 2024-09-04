9a57d0e81a9b0083eb1bf3902eb3cd0dd5e950fd
Abdollah Jalilian <stat4aj@gmail.com>
2024-09-04 13:32:13 +0100

    Update gha_sentinel_comparison.R

--- Ghana/codes/gha_sentinel_comparison.R
+++ Ghana/codes/gha_sentinel_comparison.R
@@ -9,3 +9,5 @@ sdata[[1]] <- read_excel("~/Downloads/Ghana/Control sites HLC.xlsx",
                          sheet="Sheet 1 - events 2")
 sdata[[2]] <- read_excel("~/Downloads/Ghana/Control sites CID.xlsx",
                          sheet="Sheet 1 - events", skip=1)
+
+sdata[[1]] %>% count(`Datat element name`)
