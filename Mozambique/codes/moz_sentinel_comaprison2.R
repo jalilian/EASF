d210d1788195fc6a58ca3d610ecc8c8081a556b5
jalilian <stat4aj@gmail.com>
2025-02-14 14:14:55 +0000

    Update moz_sentinel_comaprison2.R

--- Mozambique/codes/moz_sentinel_comaprison2.R
+++ Mozambique/codes/moz_sentinel_comaprison2.R
@@ -89,7 +89,7 @@ adata <- adata %>%
                              .default=latitude),
          longitude=longitude2,
          latitude=latitude2) %>%
-  select(longitude2, latitude2)
+  select(-c(longitude2, latitude2))
 
 library("mapview")
 library("sf")
@@ -147,11 +147,257 @@ dat <- adata %>%
   mutate(y = replace_na(y, 0))
 
 
+# land cover, population density and elevation data
+source("https://raw.githubusercontent.com/jalilian/CEASE/main/Ethiopia/codes/get_land_covars_africa.R")
+covars <- get_covars(dat %>% 
+                       select(longitude, latitude) %>% 
+                       as.matrix(),
+                     path="~/Downloads/Africa_covars/")
+names(covars) <- c("land_cover", "pop_density", "elevation")
+covars <- covars %>%
+  mutate(land_cover=c("Tree cover areas",
+                      "Shrubs cover areas", 
+                      "Grassland", 
+                      "Cropland",
+                      "Vegetation aquatic or regularly flooded",
+                      "Lichens Mosses / Sparse vegetation",
+                      "Bare areas",
+                      "Built-up areas",
+                      "Snow and/or Ice",
+                      "Open Water")[land_cover])
+
+dat <- bind_cols(dat, covars)
+
+saveRDS(dat, file="dat.rds")
+
+dat <- readRDS("dat.rds")
+
 source("https://raw.githubusercontent.com/jalilian/CEASE/refs/heads/main/Ethiopia/codes/get_modis.R")
 
-aa <- get_modis(what=dat %>% 
+aa <- get_modis("modis-11A2-061",
+                c("LST_Day_1km", "LST_Night_1km"),
+                what=dat %>% 
                   distinct(longitude, latitude) %>% 
                   as.matrix(),
-                datetime="2023-10-01/2024-08-30", 
+                datetime="2022-10-01/2024-08-30", 
                 aggregate=TRUE, fill_in=TRUE)
 
+dat <- dat %>%
+  rename("long"="longitude", "lat"="latitude", "date"="date3") %>%
+  left_join(aa,
+            by=join_by(date, long, lat))
+
+aa <- get_modis("modis-17A2H-061",
+                c("Gpp_500m", "PsnNet_500m"),
+                what=dat %>% 
+                  distinct(long, lat) %>% 
+                  as.matrix(),
+                datetime="2022-10-01/2024-08-30", 
+                aggregate=TRUE, fill_in=TRUE)
+
+dat <- dat %>%
+  left_join(aa,
+            by=join_by(date, long, lat))
+
+aa <- get_modis("modis-09A1-061",
+                c("sur_refl_b07"),
+                what=dat %>% 
+                  distinct(long, lat) %>% 
+                  as.matrix(),
+                datetime="2022-10-01/2024-08-30", 
+                aggregate=TRUE, fill_in=TRUE)
+
+dat <- dat %>%
+  left_join(aa,
+            by=join_by(date, long, lat))
+
+aa <- get_modis("modis-15A2H-061",
+                c("Lai_500m", "Fpar_500m"),
+                what=dat %>% 
+                  distinct(long, lat) %>% 
+                  as.matrix(),
+                datetime="2022-10-01/2024-08-30", 
+                aggregate=TRUE, fill_in=TRUE)
+
+dat <- dat %>%
+  left_join(aa,
+            by=join_by(date, long, lat))
+
+aa <- get_modis("modis-13Q1-061",
+                c("250m_16_days_EVI", "250m_16_days_NDVI"),
+                what=dat %>% 
+                  distinct(long, lat) %>% 
+                  as.matrix(),
+                datetime="2022-10-01/2024-08-30", 
+                aggregate=TRUE, fill_in=TRUE)
+
+dat <- dat %>%
+  left_join(aa,
+            by=join_by(date, long, lat))
+
+aa <- get_modis( "modis-14A2-061",
+                c("FireMask"),
+                what=dat %>% 
+                  distinct(long, lat) %>% 
+                  as.matrix(),
+                datetime="2022-10-01/2024-08-30", 
+                aggregate=TRUE, fill_in=TRUE)
+
+dat <- dat %>%
+  left_join(aa,
+            by=join_by(date, long, lat))
+
+
+
+dat <- dat %>%
+  rename_with(~ gsub("__focal", "", .))
+
+colnames(dat)
+
+dat <- dat %>%
+  rename("EVI_16_days"="250m_16_days_EVI",
+         "NDVI_16_days"="250m_16_days_NDVI")
+
+saveRDS(dat, file="dat.rds")
+# =========================================================
+dat <- readRDS("dat.rds")
+
+dat <- dat %>%  
+  mutate(tidx=as.numeric(factor(as.Date(paste(date, "01", sep="-")))),
+             sidx=as.numeric(factor(paste(long, lat, sep="-"))))
+  
+dat %>% count(date, tidx)
+
+dat <- dat %>%
+  mutate(across(pop_density:FireMask, ~ scale(.x))) 
+
+library("INLA")
+library("fmesher")
+library("sf")
+
+
+fitfun <- function(dat0, max.edge=0.2)
+{
+  mesh <- fm_mesh_2d_inla(loc=dat0[, c("long", "lat")], 
+                          max.edge=max.edge)
+  #spde <- inla.spde2.matern(mesh, alpha=2)
+  spde <- inla.spde2.pcmatern(
+    mesh, alpha=2,
+    # lower tail quantile and probability for the range 
+    prior.range = c(0.1, 0.025),
+    # upper tail quantile and probability for the standard deviation
+    prior.sigma = c(10, 0.025)
+  )
+  
+  fm <- y ~  elevation + pop_density +
+    f(tidx, model="iid", constr=TRUE) + 
+    f(mesh$idx$loc, model=spde) +
+    #f(sidx, model="iid", constr=TRUE) +
+    land_cover + pop_density + elevation +
+    LST_Day_1km + LST_Night_1km + Gpp_500m +
+    PsnNet_500m + sur_refl_b07 + Lai_500m +
+    Fpar_500m + 
+    EVI_16_days + NDVI_16_days +
+    FireMask
+  inla(fm, data=dat0, family="nbinomial",
+       #control.family(control.link=list(model="log")),
+       #control.compute=list(return.marginals.predictor=TRUE),
+       control.predictor=list(compute=TRUE, link=1), 
+       #control.inla=list(strategy="laplace", npoints=21),
+       #control.inla=list(fast=FALSE, strategy="laplace", dz=0.25, h=1e-5)
+       control.compute=list(config=TRUE, dic=TRUE, waic=TRUE),
+       silent=1L, num.threads=1)
+}
+
+fit1 <- fitfun(dat %>% filter(programme == "Routine"))
+fit1$summary.fixed
+
+
+fit2 <- fitfun(dat %>% filter(programme == "easf"))
+round(fit2$summary.fixed[, c(1, 3, 5)], 2)
+fit2$summary.hyperpar
+
+cvfun <- function(dat, newdat=NULL, max.edge=0.25, mc.cores=4)
+{
+  if (is.null(newdat))
+  {
+    idx <- 1:nrow(dat)
+  } else{
+    idx <- nrow(dat) + 1:nrow(newdat)
+    dat <- bind_rows(dat, newdat)
+  }
+  fitifun <- function(i)
+  {
+    dati <- dat
+    dati$y[i] <- NA
+    fiti <- fitfun(dati, max.edge=max.edge)
+    unlist(fiti$summary.fitted.values[i, c(1, 2)])
+  }
+  out <- parallel::mclapply(idx, fitifun, mc.cores=mc.cores)
+  out <- matrix(unlist(out), ncol=2, byrow=TRUE)
+  out <- cbind(out, dat$y[idx])
+  return(out)
+}
+
+cv1 <- cvfun(dat %>% filter(programme == "Routine"))
+sqrt(mean(((cv1[, 1] - dat %>% filter(programme == "Routine") %>% pull(y)) / 
+             cv1[, 2])^2))
+
+
+nsim <- 499 
+mc.cores <- 6
+cv2s <- cv21s <- cv12s <- matrix(NA, nrow=nsim, ncol=2)
+for (j in 1:nsim)
+{
+  routine_dat <- dat %>%
+    filter(program == "Routine")
+  
+  nested_dat <- dat %>%
+    filter(program == "EASF")%>% 
+    group_by(year, month, region) %>%  
+    nest() %>%            
+    ungroup() %>% 
+    left_join(
+      routine_dat %>% 
+        group_by(year, month, region) %>% 
+        count(year, month, region),
+      by = join_by(year, month, region)
+    ) %>%
+    na.omit()
+  
+  samp_dat <- nested_dat %>%
+    mutate(samp = map2(data, n, sample_n, replace=TRUE)) %>%
+    select(-data) %>%
+    unnest(samp)
+  
+  cv2 <- cvfun(samp_dat, newdat=NULL, max.edge=NULL, mc.cores=mc.cores)
+  cv21 <- cvfun(samp_dat, newdat=routine_dat, max.edge=NULL, mc.cores=mc.cores)
+  cv12 <- cvfun(routine_dat, newdat=samp_dat, max.edge=NULL, mc.cores=mc.cores)
+  
+  if (is.numeric(cv2) & is.numeric(cv21) & is.numeric(cv12))
+  {
+    cv2s[j, 1] <- mean(((cv2[, 3] - cv2[, 1]) / (1 + cv2[, 3]))^2)
+    cv2s[j, 2] <- mean(((cv2[, 3] - cv2[, 1]) / cv2[, 2])^2)
+    cv21s[j, 1] <- mean(((cv21[, 3] - cv21[, 1]) / (1 + cv21[, 3]))^2)
+    cv21s[j, 2] <- mean(((cv21[, 3] - cv21[, 1]) / cv21[, 2])^2)
+    cv12s[j, 1] <- mean(((cv12[, 3] - cv12[, 1]) / (1 + cv12[, 3]))^2)
+    cv12s[j, 2] <- mean(((cv12[, 3] - cv12[, 1]) / cv12[, 2])^2)
+  }
+  cat("\nj: ", j, "\t", cv2s[j, ], "\t", cv21s[j, ], "\t", cv12s[j, ], "\n")
+}
+
+mean(sqrt(cv2s[, 2]), na.rm=TRUE)
+quantile(sqrt(cv2s[, 2]), c(0.025, 0.975), na.rm=TRUE)
+
+
+mean(sqrt(cv21s[, 2]), na.rm=TRUE)
+quantile(sqrt(cv21s[, 2]), c(0.025, 0.975), na.rm=TRUE)
+
+mean(sqrt(cv12s[, 2]), na.rm=TRUE)
+quantile(sqrt(cv12s[, 2]), c(0.025, 0.975), na.rm=TRUE)
+
+
+mean(sqrt(cv21s[, 2]) <= mean(sqrt(cv12s[, 2])))
+mean(sqrt(cv12s[, 2]) <= mean(sqrt(cv21s[, 2])))
+
+# =========================================================
